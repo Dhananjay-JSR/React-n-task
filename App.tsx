e9a916f201050836480d195b99bfe8d87fa9d3f5
Dhananjay-JSR <ajaysenday@gmail.com>
2024-02-28 16:02:03 +0530

    Setup NativeWind

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// ignore ts error
+declare module "react-native" {
+  interface ViewProps {
+    className?: string;
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-yellow-300">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
